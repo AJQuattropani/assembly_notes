cc11b02b229a9d074f32b1d174c3df43474b4200
AJ Quattropani <AJQuattropani@github.com>
2025-05-14 15:13:13 -0600

    main.asm outputs printed values in binary, decimal, and hex of the value specified in the data section.

--- main.asm
+++ main.asm
@@ -1,50 +1,25 @@
+%include "putint.asm"
+
+%macro exit 1
+  push %1
+  jmp _exit
+%endmacro
 
 section .text
-  global _start, _strlen
-_strlen: ; take rdi as an argument
-  ; prefix
-  push rbp
-  mov rbp, rsp
-  sub rsp, 0 ; uses no stack space
-  push rbx
-  push r12
-  push r13
-  push r14
-  push r15
-  ; body
-  mov rcx, -1
-  xor al, al
-  repnz scasb ; count until val in rdi = \0
-  mov rax, -2
-  sub rax, rcx ; save length in rax
-  mov rcx, -1
-  ; postfix
-  pop r15
-  pop r14
-  pop r13
-  pop r12
-  pop rbx
-  mov rsp, rbp ; reset the stack
-  pop rbp
-  ;pop rip
-  ret
-_start:
-  mov rdi, stat_str
-  call _strlen
-  mov rdi, rax
-  mov rax, stat_len
-  sub rax, 1
-  sub rdi, rax
-  push 0x3c ; exit code 60, exit
-  pop rax
-  syscall
+  global _start, _putint_hex, _exit
 _exit:
-  push 0x3c ; exit code 60, exit
+  push 0x3c ; syscall 60
   pop rax
-  xor rdi, rdi
+  pop rdi
   syscall
+_start:
+  putint_dec [NUM]
+  putchar byte 0xa
+  putint_hex [NUM]
+  putchar byte 0xa
+  putint_bin [NUM]
+  exit 0
+_main:
 section .data
 section .rodata
-  stat_str db "Hello, World!",0xA,0x0
-  stat_len equ $ - stat_str
-section .bss
+  NUM dq 0xfedcba9876543210 ; should match output
